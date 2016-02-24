0b2b70de3f5625cab9ebcd4c5d4faf80457ad725
David Fekke <david@fekke.com>
2016-02-24 13:10:28 -0500

    Making Windows changes to client

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -1,24 +1,29 @@
-import React from 'react/addons';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag,
+  Simulate
+} from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
-const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} 
-    = React.addons.TestUtils;
-
 describe('Voting', () => {
-  
-  it('renders a pair of buttons', () => 
+
+  // ...
+
+  it('invokes callback when a button is clicked', () => {
     let votedWith;
     const vote = (entry) => votedWith = entry;
-    
+
     const component = renderIntoDocument(
-      <Voting pair={["Trainspotting", "28 Days Later"]} 
-              vote={vote} />
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              vote={vote}/>
     );
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
     Simulate.click(buttons[0]);
 
     expect(votedWith).to.equal('Trainspotting');
   });
-  
+
 });
